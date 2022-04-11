24b642a2f876afd4f9a1e60c56eca8d63eac4bda
TrungThanh1107 <thanh.ntr.61cntt@ntu.edu.vn>
2022-04-11 14:55:41 +0700

    Add new

--- app/src/main/java/NguyenTrungThanh/BaiKT2/MainActivity.java
+++ app/src/main/java/NguyenTrungThanh/BaiKT2/MainActivity.java
@@ -3,10 +3,12 @@ package NguyenTrungThanh.BaiKT2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +18,35 @@ public class MainActivity extends AppCompatActivity {
     Button btn =(Button) findViewById(R.id.btn);
     TextView ketqua=(TextView) findViewById(R.id.ketqua);
     EditText edit =(EditText) findViewById(R.id.edit);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               float sotien=Float.parseFloat(edit.getText().toString());
+               if(rd13.isChecked())
+               {
+                 float tip=(float) (sotien*0.13);
+                 ketqua.setText("Your tip will be $"+tip);
+                 Toast.makeText(MainActivity.this, "Your tip will be $"+tip, Toast.LENGTH_SHORT).show();
+               }
+               if(rd15.isChecked())
+               {
+                   float tip=(float) (sotien*0.15);
+                   ketqua.setText("Your tip will be $"+tip);
+                   Toast.makeText(MainActivity.this, "Your tip will be $"+tip, Toast.LENGTH_SHORT).show();
+               }
+               if(rd18.isChecked())
+               {
+                   float tip=(float) (sotien*0.18);
+                   ketqua.setText("Your tip will be $"+tip);
+                   Toast.makeText(MainActivity.this, "Your tip will be $"+tip, Toast.LENGTH_SHORT).show();
+               }
+           }
+       });
     }
 }
