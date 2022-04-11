173dd2b4cd6b15bd6d6d1dbc45a4c86aeefb5c04
TrungThanh1107 <thanh.ntr.61cntt@ntu.edu.vn>
2022-04-11 14:48:01 +0700

    Add new

--- app/src/main/java/NguyenTrungThanh/BaiKT2/MainActivity.java
+++ app/src/main/java/NguyenTrungThanh/BaiKT2/MainActivity.java
@@ -3,9 +3,19 @@ package NguyenTrungThanh.BaiKT2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioButton rd13=(RadioButton) findViewById(R.id.radio1);
+    RadioButton rd15=(RadioButton) findViewById(R.id.radio2);
+    RadioButton rd18=(RadioButton) findViewById(R.id.radio3);
+    Button btn =(Button) findViewById(R.id.btn);
+    TextView ketqua=(TextView) findViewById(R.id.ketqua);
+    EditText edit =(EditText) findViewById(R.id.edit);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
